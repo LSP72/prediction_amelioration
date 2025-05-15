3299b46eaae7a18fcdbb639a1e9b726e0ac74dde
Math <math.math@caramail.fr>
2025-05-15 00:11:47 -0400

    Adding the plotting part

--- Multiple_SVR.py
+++ Multiple_SVR.py
@@ -78,189 +78,34 @@ print("Kurtosis test p-values:\n", kurtst.pvalue)
 # print("Kurtosis values:\n", kurtst.statistic)
 # print("Kurtosis test p-values:\n", kurtst.pvalue)
 
-# --------------- ML algorithm: Support Vector Machine (SVM) ---------------
-# ----- Select features based on MRMR algorithm ------
-features = pd.read_excel(r'/Users/mathildetardif/Documents/Python/Biomarkers/responder_prediction/functions/Features.xlsx')
-selectedFeatures = features['18']
-selectedFeatures = selectedFeatures.dropna()
-# selectedFeatures = featureSelection.selector(allfeatures=all_data, label=MCID, number_of_important_features= len(all_data.columns))
-selected_data_df = all_data[selectedFeatures]
+## ----- Looking for the different predictions -----
 
-# correlation_matrix = selected_data.corr()
-# sns.heatmap(correlation_matrix, annot= True, cmap = 'coolwarm')
-# plt.show()
-selected_data = selected_data_df.values
+# X_pp = row with all the info of one participant
 
-# ----- Train / test Split -----
-# Going to split the data for training/testing
-x_train, x_test, y_train, y_test = train_test_split(selected_data, MCID, test_size = 0.25, random_state=72, stratify=MCID)
-                                                    #inputs     #outputs #size of test set #making the split reproductible #ensure proportions are same for train/test sets
-# ----- Scale the data -----
-# normalizer = MinMaxScaler()
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train) # standardise/normalise the data based in the input by calculatG mean & SD of x_train & then, stdise x_train
-x_test = scaler.transform(x_test)       # standardise x_test
+vitesse_pred = SVR_vitesse.prediction_vitesse(X_pp, all_data)
+_6MWT_pred = SVR_6MWT.prediction_6MWT(X_pp, all_data)
 
-# Applying SMOTE
-smote_train = SMOTE(sampling_strategy='auto',   random_state=42,                n_jobs=-1)
-#                                was = {1:27,0:20}
-#ratio of the min class to the maj after resampling # ctrtls random°/ensurG reprodY #nb of // jobs for computation
-# maj needs to be changed regarding the size of the sample
-# 21p => 24 /// 23p => 27
-x_train, y_train = smote_train.fit_resample(x_train, y_train) # resample the data sets to have new 'balanced' data sets
+## Getting the pre-values
+pre_vitesse = X_pp['VIT_PRE']
+pre_6MWT = X_pp['6MWT_PRE']
 
+temp = ['PRE', 'POST'] 
+vitesse = [pre_vitesse, vitesse_pred]
+vitesse_progression = 100*(vitesse_pred-pre_vitesse)/pre_vitesse
+_6MWT = [pre_6MWT, _6MWT_pred]
+_6MWT_progression = 100*(_6MWT_pred-pre_6MWT)/pre_6MWT
 
-# ----- Bayesian Optimization -----
-# Method to find the best paramaters for a model (minG/maxG an objve f°)
-
-pbounds = {
-            'C': (1,1000),
-            'gamma': (0.001, 1),
-            'degree': (2,5),
-            'kernel_idx':(0,3)
-            }
-kernels = ['linear', 'poly', 'rbf', 'sigmoid']
-
-errors = []
-BP = {}
-SVM_acc = {} 
-
-def svm_model(C, gamma, degree, kernel_idx):
-    kernel = kernels[int(round(kernel_idx))]
-    SVM = svm.SVC(kernel=kernel, C=C, gamma=gamma, degree=int(degree), random_state=42)
-    
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-    # cv_LOO = LeaveOneOut()
-    acc = cross_val_score(SVM, x_train, y_train, cv=cv, scoring = 'accuracy')
-    return acc.mean()
-    
-print('************************************')
-print('Bayesian Optimization initiated.')
-
-optimizer = BayesianOptimization(f = svm_model, pbounds = pbounds,                random_state=42, verbose=3)
-                          # the objve f° #a dict specifiG range for each param #for reprodY   #ctrls amount of info displayed 
-optimizer.maximize(init_points=10, n_iter=100) # For 10 features accuracy:91
-results = pd.DataFrame(optimizer.res)
-
-# param_grid = {
-#     'C' :(1,1000),
-#     'gamma': (0.001, 1),
-#     'degree': (2,5),
-#     'kernel' : ['linear', 'poly', 'rbf', 'sigmoid']
-#     }
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.svm import SVC
-# svm = SVC()
-# grid_search = GridSearchCV(svm, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
-# grid_search.fit(x_train, y_train)
-# best_svm = grid_search.best_estimator
-# print("Best Parameters:", grid_search.best_params_)
-# print("Best Score:", grid_search.best_score_)
-
-# best_parameters = optimizer.max['params']
-# best_parameters['C'] = float(best_parameters['C'])
-# best_parameters['degree'] = int(best_parameters['degree'])
-# kernel = kernels[int(round(best_parameters['kernel_idx']))]
-# print("Best hyperparameters found:", best_parameters," \n and the kernel is: ", kernel, ".")
-# BP[kernel] = optimizer.max 
-
-   # Train the final SVM model with the best parameters
-SVM_best = svm.SVC(kernel=kernel, C=best_parameters['C'], gamma=best_parameters['gamma'], degree=best_parameters['degree'], random_state=42)
-SVM_best.fit(x_train, y_train)
-    
-y_hat = SVM_best.predict(x_test)
-
-# Evalutation of the model on the test set
-SVM_acc[kernel] = accuracy_score(y_test, y_hat)
-mse = mean_squared_error(y_test, y_hat)
-errors.append({kernel: mse})
-print(f"Final Test Accuracy for {kernel}: {SVM_acc[kernel]}")
-print("Test set score: ", SVM_best.score(x_test, y_test))
-print(metrics.classification_report(y_test, y_hat))
-    # y_probs = SVM_best.decision_function(x_test)
-    # print("AUC value: ", roc_auc_score(y_test, y_probs))
-
-# ----- Parameter testing to CSV -----
-# # If want to recover all the parameters that have been tested during the 5CV 
-# # for each kernel 
-# # TO ADD IN THE LOOP
-# params = pd.DataFrame(results['params'].to_list())
-# results = pd.concat([results['target'], params, pd.DataFrame([best_parameters])], axis = 1)
-# results.to_csv('/Users/mathildetardif/Library/CloudStorage/OneDrive-UniversitedeMontreal/Mathilde Tardif - PhD - Biomarkers CP/PhD projects/Training responders/MyScripts/Bayesian_Optim_%s.csv' %(kernel))
-
-
-
-# Recovery of the best parameters
-only_BP = {}
-for kernel in kernels:
-    only_BP[kernel] = BP[kernel]['params']
-
-only_BP_df = pd.DataFrame(only_BP, index = ['C', 'degree', 'gamma'])
-only_BP_df.to_csv('/Users/mathildetardif/Library/CloudStorage/OneDrive-UniversitedeMontreal/Mathilde Tardif - PhD - Biomarkers CP/PhD projects/Training responders/MyScripts/BestParameters_%s.csv' %(smote_train.sampling_strategy))
-
-# mean_error = np.mean(errors)
-# print(f'Average Mean Squared Error: {mean_error}')
-
-
-# ----- Confusion Matrix -----
-labels = ['Non-responder', 'Responder']
-kernel = 'rbf'
-confusion_mat = confusion_matrix(y_true=y_test, y_pred=y_hat)
-disp = ConfusionMatrixDisplay(confusion_mat, display_labels=labels)
-disp.plot()
-plt.title('Confusion Matrix for %s Kernel' %(kernel))
+# Plotting the evolution
+fig, axs = plt.subplots(2)
+fig.suptitle('Evolution pre/post')
+axs[0].plot(temp, vitesse)
+for x, y in zip(temp, vitesse):
+    axs[0].text(x, y, f"{y:.2f}", ha='center', va='bottom', fontsize=8)
+axs[0].text(0.5, 0.1, f"Evolution of {vitesse_progression:.2f} %", transform=axs[0].transAxes, fontsize=10, color='gray', ha='center')
+axs[0].set_title('Vitesse')
+axs[1].plot(temp, _6MWT)
+for x, y in zip(temp, _6MWT):
+    axs[1].text(x, y, f"{y:.2f}", ha='center', va='bottom', fontsize=8)
+axs[1].text(0.5, 0.1, f"Evolution of {_6MWT_progression:.2f} %", transform=axs[1].transAxes, fontsize=10, color='gray', ha='center')
+axs[1].set_title('6MWT')
 plt.show()
-TN, FP, FN, TP = confusion_mat.ravel()
-print(f"Vrais positifs (TP) : {TP}")  # Bien classés comme positifs
-print(f"Faux positifs (FP) : {FP}")  # Négatifs classés à tort comme positifs
-print(f"Faux négatifs (FN) : {FN}")  # Positifs classés à tort comme négatifs
-print(f"Vrais négatifs (TN) : {TN}")  # Bien classés comme négatifs
-
-
-# ----- Calculate SHAP values for the best non-linear model -----
-explainer = shap.KernelExplainer(SVM_best.predict, x_train)
-shap_values = explainer.shap_values(x_test)
-base_value = explainer.expected_value  
-# if not isinstance(shap_values, shap.Explanation):
-#     shap_values = shap.Explanation(shap_values, feature_names=selectedFeatures)
-
-# Plot SHAP values
-shap.summary_plot(shap_values, x_test, feature_names=selectedFeatures)
-
-
-
-## Graphs
-# ----- Waterfall plots -----
-for i in range(len(shap_values)):
-    shap_exp = shap.Explanation(        # will transform numpy.arry in explanation obj
-        values=shap_values,          # SHAP values for the first instance
-        base_values=base_value,        # Baseline prediction
-        data=x_test,                    # Feature values for the instance
-        feature_names=selectedFeatures) # Custom feature names
-    fig, ax = plt.subplots()
-    shap.waterfall_plot(shap_exp[i], show = False)
-    plt.title("Waterfall plot for patient %s" %(y_test.index[i]), fontsize=16, pad=20)  # Customize title
-    plt.show()
-
-# ----- Bar plot-----
-# To use if shap_values has been transformed in an Explanation Object
-if not isinstance(shap_values, shap.Explanation):
-     shap_values = shap.Explanation(shap_values, feature_names=selectedFeatures)
-shap_values.feature_names = selectedFeatures
-shap.plots.bar(shap_values, max_display=len(shap_values[0]))
-plt.show()                              # IF shap_values is an Explanation Object
-                                        # => no need of ".data"
-
-# ----- Beeswarm plot -----
-# summary one
-
-# ----- Dependance plot -----
-shap.plots.scatter(shap_values)                         # for all features
-shap.plots.scatter(shap_values[:,'pctSimpleAppuie'])    # for specific features
-
-# ----- Decision plot highlighting misclassified -----
-select = range(len(y_test))
-y_pred = (shap_values.sum(1) + base_value) > 0
-misclassified = y_pred != y_test.iloc[select]
-shap.decision_plot(base_value, shap_values, selectedFeatures, link="logit", highlight=misclassified)
-
