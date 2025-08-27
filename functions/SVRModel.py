b06d982a18be1374de27570716f99b6c73c923c1
Math <math.math@caramail.fr>
2025-08-27 17:27:27 -0400

    SVRModel > adding perf_estimate
    Function to estimate the reliability of the model and choose if the model is ok

--- functions/SVRModel.py
+++ functions/SVRModel.py
@@ -28,6 +28,51 @@ class SVRModel:
         else:                       # if already with something in, will append the new sample
             self.X = pd.concat([self.X, X], ignore_index=True)
             self.y = pd.concat([self.y, y], ignore_index=True)
+            
+    def perf_estimate(self, n_iter):
+        """Check for the overall perf of the model with nested CV method"""
+        
+        if self.X is None or self.y is None:        # Check if there is some data
+            raise ValueError("No data available for training.")
+            
+        inner_cv = KFold(n_splits=5, shuffle=True, random_state=42)
+        outer_cv = KFold(n_splits=5, shuffle=True, random_state=72)
+        
+        print(f"🔍 Starting hyperparameter search...")
+
+        # Define search space
+        pbounds = {
+            "svr__C": uniform(1, 500),
+            "svr__epsilon": uniform(0.01, 1),
+            "svr__kernel": ["linear", "poly", "rbf"],
+            "svr__gamma": ["scale", "auto"]     # "scale" = 1/(n_features * X.var())
+                                                # "auto" = 1/n_features 
+        }
+
+        # Create a pipeline for the model: scaling + SVR - everydata will pas through that order
+        pipeline = Pipeline([
+            ("scaler", StandardScaler()),
+            ("svr", SVR())
+            ])
+    
+        # Creating the optimisation loop 
+        search = RandomizedSearchCV(
+            pipeline,
+            param_distributions=pbounds,
+            n_iter=n_iter,
+            scoring="neg_mean_squared_error",               # will try to maximise r2
+            cv=inner_cv,
+            random_state=72,
+            verbose=2,
+            n_jobs=1
+        )
+        
+        cv_r2 = cross_val_score(search, self.X, self.y, cv=outer_cv, scoring="r2")
+        cv_rmse = np.sqrt(-cross_val_score(search, self.X, self.y, cv=outer_cv, scoring="neg_mean_squared_error"))
+        print(f"📊 CV R²: {cv_r2.mean():.4f} ± {cv_r2.std():.4f}")
+        print(f"📊 CV RMSE: {cv_rmse.mean():.4f} ± {cv_rmse.std():.4f}")
+        
+        return cv_r2.mean(), cv_rmse.mean()
 
     def train_and_tune(self, n_iter=100):
         """Tune hyperparameters with RandomizedSearchCV + LOO CV."""
@@ -61,7 +106,7 @@ class SVRModel:
             scoring="neg_mean_squared_error",               # will try to maximise r2
             cv=loo,
             random_state=72,
-            verbose=1,
+            verbose=2,
             n_jobs=1
         )
 
@@ -69,7 +114,7 @@ class SVRModel:
 
         self.model = search.best_estimator_         # recover the best model
         self.best_params_ = search.best_params_     # recover the best hp
-        print("✅ Training complete.")
+        print("✅ Optimisation completed and model trained.")
 
         # Evaluate
         preds = self.model.predict(self.X)          # quick check to see if model OK (no overfitting)
