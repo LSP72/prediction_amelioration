bdf3fcdb3a09465b5efcb5107aba7f3edd09107d
Math <math.math@caramail.fr>
2025-08-27 23:24:29 -0400

    SVRModel > describing the function fit

--- functions/SVRModel.py
+++ functions/SVRModel.py
@@ -142,7 +142,11 @@ class SVRModel:
 
   
     def fit(self, X, y):
-        """Train the model with the (X, y) dataset"""
+        """
+        Train the model with the (X, y) dataset
+        Actually, refits the current optimized model on a new dataset,
+        without hyperparameter tuning.
+        """
         if self.model is None:
             raise ValueError("Model has not been optimised yet.")
         return self.model.fit(X, y)
