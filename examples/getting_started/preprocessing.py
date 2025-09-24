e59d400e0c23491ea3418ce479348fdedc5c0b82
Math <math.math@caramail.fr>
2025-09-24 07:00:43 -0400

    examples > function to create the matrix use after

---
+++ examples/getting_started/preprocessing.py
@@ -0,0 +1,24 @@
+from amelio_cp import Process
+
+"""
+    This script goes and finds the raw data in differents formats (.mat, .csv, .xlsx),
+    and processes them to create a single dataframe with all the features and label.
+
+    For now, it looks for:
+        - Kinematic data in .mat files (one per patient)
+        - GPS data in a .csv file
+            • This files is created by calculating the GPS values from the kinematic data.
+        - Demographic data in an .xlsx file
+"""
+output_dir = 'datasets/sample_1/processed_data'
+data_dir = 'datasets/sample_1/raw_data'
+gps_path = 'datasets/sample_1/GPS_25pp.csv'
+demographic_path = 'datasets/sample_1/6MWT_speed_25pp_caracteristics.xlsx'
+
+all_data = Process().load_data(data_dir=data_dir,
+                             output_dir=output_dir,
+                             gps_path=gps_path,
+                             demographic_path=demographic_path,
+                             separate_legs=True)
+
+print(all_data)
--- examples/getting_started/svc_vitesse.py
+++ examples/getting_started/svc_vitesse.py
@@ -3,7 +3,7 @@ from amelio_cp import Process
 from amelio_cp import SVCModel
 
 # %% Collecting/Loading the data from a csv file already created
-file_path = "dataset/sample_1/all_data.csv"
+file_path = "datasets/sample_1/all_data.csv"
 
 all_data = Process.load_csv(file_path)
 
@@ -21,4 +21,4 @@ SVC_VIT = SVCModel()
 SVC_VIT.add_data(data, delta_VIT)
 SVC_VIT.train_and_tune("random", n_iter=100)
 
-print(SVC_VIT.best_params_)
+print(SVC_VIT.best_params)
