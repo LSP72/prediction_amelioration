00948604a0573e9a1278b8f39b0f239989cb7d38
Math <math.math@caramail.fr>
2025-10-24 18:03:21 -0400

    black formating

--- tests/shard1/test_models.py
+++ tests/shard1/test_models.py
@@ -12,6 +12,7 @@ data_path = "examples/sandbox/fake_data_for_test.csv"
 data = Process.load_csv(data_path)
 features_path = "amelio_cp/processing/Features.xlsx"
 
+
 def test_linear_model():
     model = LinearModel()
 
@@ -64,25 +65,125 @@ def test_svr_model():
         np.array([model.model.C, model.model.gamma, model.model.epsilon, model.model.degree]),
         np.array([50, 0.01, 0.1, 4]),
     )
-    
-    X, y, _ = Process.prepare_data(data_path, features_path, 'VIT', model_name=model.name)
+
+    X, y, _ = Process.prepare_data(data_path, features_path, "VIT", model_name=model.name)
     model.add_data(X, y, test_size=0.2)
 
     np.testing.assert_equal(np.array(model.X_train.shape), np.array((80, 19)))
-    np.testing.assert_equal(np.array(model.X_train.index),
-                            np.array([ 89,  26,  42,  70,  15,  40,  72,   9,  96,  11,  91,  64,  28,
-        83,   5,  47,  53,  35,  16,  81,  34,   7,  43,  73,  27,  19,
-        94,  25,  62,  49,  13,  24,   3,  17,  38,   8,  79,   6,  65,
-        36,  88,  56, 100,  54,  50,  68,  46,  69,  61,  98,  80,  41,
-        58,  48,  90,  57,  75,  32,  95,  59,  63,  85,  37,  29,   1,
-        52,  21,   2,  23,  87,  99,  74,  86,  82,  20,  60,  71,  14,
-        92,  51]))
-    np.testing.assert_almost_equal(model.X_train_scaled[0],
-                                   np.array([-1.50658960e+00, -1.08876334e+00, -6.53742618e-01, -1.88901776e+00,
-        9.65911502e-01, -1.03916133e+00,  1.27214018e+00, -1.48061604e-01,
-        4.55436226e-04, -5.05783029e-01, -1.07370965e-01, -2.80553179e-01,
-        6.64556184e-01,  1.29468029e-01, -1.77317741e+00, -2.04696561e+00,
-       -4.34434708e-01, -9.10231720e-02,  7.50639657e-01]))
+    np.testing.assert_equal(
+        np.array(model.X_train.index),
+        np.array(
+            [
+                89,
+                26,
+                42,
+                70,
+                15,
+                40,
+                72,
+                9,
+                96,
+                11,
+                91,
+                64,
+                28,
+                83,
+                5,
+                47,
+                53,
+                35,
+                16,
+                81,
+                34,
+                7,
+                43,
+                73,
+                27,
+                19,
+                94,
+                25,
+                62,
+                49,
+                13,
+                24,
+                3,
+                17,
+                38,
+                8,
+                79,
+                6,
+                65,
+                36,
+                88,
+                56,
+                100,
+                54,
+                50,
+                68,
+                46,
+                69,
+                61,
+                98,
+                80,
+                41,
+                58,
+                48,
+                90,
+                57,
+                75,
+                32,
+                95,
+                59,
+                63,
+                85,
+                37,
+                29,
+                1,
+                52,
+                21,
+                2,
+                23,
+                87,
+                99,
+                74,
+                86,
+                82,
+                20,
+                60,
+                71,
+                14,
+                92,
+                51,
+            ]
+        ),
+    )
+    np.testing.assert_almost_equal(
+        model.X_train_scaled[0],
+        np.array(
+            [
+                -1.50658960e00,
+                -1.08876334e00,
+                -6.53742618e-01,
+                -1.88901776e00,
+                9.65911502e-01,
+                -1.03916133e00,
+                1.27214018e00,
+                -1.48061604e-01,
+                4.55436226e-04,
+                -5.05783029e-01,
+                -1.07370965e-01,
+                -2.80553179e-01,
+                6.64556184e-01,
+                1.29468029e-01,
+                -1.77317741e00,
+                -2.04696561e00,
+                -4.34434708e-01,
+                -9.10231720e-02,
+                7.50639657e-01,
+            ]
+        ),
+    )
+
 
 def test_svc_model():
     model = SVCModel()
