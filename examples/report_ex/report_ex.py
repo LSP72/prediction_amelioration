8bf3270241e0486d86033dede41318978f12dc3c
Math <math.math@caramail.fr>
2025-11-04 17:30:00 -0500

    fix(report_ex)
    fixing errors to make it work
    => works fine now

--- examples/report_ex/report_ex.py
+++ examples/report_ex/report_ex.py
@@ -1,24 +1,21 @@
 import pandas as pd
 import pickle as pkl
-from amelio_cp import RFCModel
+from amelio_cp import SVRModel
 from amelio_cp import SVCModel
 from amelio_cp import Process
-from amelio_cp import SHAPPlots
-from sklearn.metrics import r2_score, accuracy_score, confusion_matrix
 import time
 
 
-def build_model(model_name, seed=42):
+def build_model(model_name):
     if model_name == "svc":
         model = SVCModel()
 
     elif model_name == "svr":
+        model = SVRModel()
         pass
-
-    model.random_state = seed
     return model
 
-# will remove the data 20 [0] and 44 [1]
+
 def prepare_data(data_path, features_path, condition_to_predict, model_name, samples_to_keep):
     all_data = Process.load_csv(data_path)
     if condition_to_predict == "VIT":
@@ -54,26 +51,9 @@ def prepare_data(data_path, features_path, condition_to_predict, model_name, sam
     return X, y, X_ex, y_ex, features_names
 
 
-def load_data(X, y, model, X_ex, y_ex):
+def load_data(model, X, y, X_ex, y_ex):
     model.add_train_data(X, y) # not going to evaluate the model, so using all the data for training
     model.add_test_data(X_ex, y_ex)
-
-def append_data(results_dict, model, id, time, training_accuracy, precision_score, conf_matrix):
-    results_dict["id_" + str(id)] = {
-        "model_name": model.name,
-        "optim_method": model.optim_method,
-        "seed": model.random_state,
-        "C": model.best_params["C"],
-        "gamma": model.best_params["gamma"],
-        "degree": model.best_params["degree"],
-        "kernel": model.best_params["kernel"],
-        "training_accuracy": training_accuracy,
-        "precision_score": precision_score,
-        "confusion_matrix": conf_matrix,
-        "optim_time": time,
-    }
-
-    return results_dict
 
 
 def save_data(results_dict, model_name, output_path):
@@ -82,22 +62,22 @@ def save_data(results_dict, model_name, output_path):
         pkl.dump(results_dict, file)
 
 
-def main(condition_to_predict):
+def main(condition_to_predict, samples_to_keep):
     results_dict = {}
     data_path = "datasets/sample_2/all_data_28pp.csv"
     features_path = "amelio_cp/processing/Features.xlsx"
-    output_path = "examples/results/svc_vs_svr_rdm_state/"
+    output_path = "examples/results/report_ex/"
     output_path_shap = output_path + "shap_fig/"
     models = ['svc', 'svr']
 
-    for model_name in enumerate(models):
+    for model_name in models:
         starting_time = time.time()
 
         model = build_model(model_name)
-        X, y, X_ex, y_ex, features_names = prepare_data(data_path, features_path, condition_to_predict, model_name)
-        load_data(X, y, X_ex, y_ex, model)
+        X, y, X_ex, y_ex, features_names = prepare_data(data_path, features_path, condition_to_predict, model_name, samples_to_keep)
+        load_data(model, X, y, X_ex, y_ex)
 
-        model.model.train_and_tune(model.X_train_scaled, model.y_train)
+        model.train_and_tune("bayesian_optim", n_iter=20)
 
         y_pred = model.model.predict(model.X_test_scaled)
 
@@ -112,9 +92,10 @@ def main(condition_to_predict):
             "optim_time": optim_time
             }
 
-    save_data(results_dict, model_name, output_path)
+    save_data(results_dict, condition_to_predict, output_path)
 
 
 if __name__ == "__main__":
-    main("VIT")
-    main("6MWT")
+    samples_to_keep = [20, 44] # will remove the data 20 [0] and 44 [1]
+    main("VIT", samples_to_keep)
+    # main("6MWT")
