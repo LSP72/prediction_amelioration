01bab756049e4e9fb4af6b210034fdc11c0c703a
Math <math.math@caramail.fr>
2025-11-04 00:40:14 -0500

    to fix(test_optimisation): issue w/ hyperparams
    Nos the same with same way to call.use it...

--- tests/shard1/test_optimisation.py
+++ tests/shard1/test_optimisation.py
@@ -16,16 +16,26 @@ def test_optimisation_svc_model():
     X, y, _ = Process.prepare_data(data_path, features_path, "VIT", model_name=model.name)
     model.add_data(X, y, test_size=0.2)
 
-    model.train_and_tune("bayesian_optim")
+    model.train_and_tune("bayesian_optim", n_iter=50)
     y_pred = model.model.predict(model.X_test_scaled)
 
     np.testing.assert_equal(
-        np.array([model.model.C, model.model.gamma, model.model.degree]),
-        np.array([22.00829813345192, 0.001, 2])
+        np.array([ model.model.gamma, model.model.degree]),
+        np.array([ 0.001, 3])
         )
     
-    # np.testing.assert_equal(
-    #     np.array(y_pred),
-    #     np.array([0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1])
-    #     )
+    np.testing.assert_equal(
+        np.array(y_pred),
+        np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        )
+
+    model.train_and_tune("bayesian_search", n_iter=10)
+    np.testing.assert_equal(
+        np.array([model.model.C, model.model.gamma, model.model.degree]),
+        np.array([410.6938548944605, 0.09335393188593556, 4])
+        )
     
+    np.testing.assert_equal(
+        np.array(y_pred),
+        np.array([0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0])
+                 )
